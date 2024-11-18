c37078460ab54e93b8f1321239c93fff5b39ea8e
Gustavo <gc722793@gmail.com>
2024-11-18 14:50:42 -0300

    fix eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-      images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'github.com',
-            port: '',
-            pathname: '/gusfngg.png',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/gusfngg.png',
       },
-};
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
